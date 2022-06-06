775149d904d87f1e1d9b54dcf43d388cb98f0325
sagar kadam <misc.sagarkadam@gmail.com>
2022-06-06 10:27:48 +0530

    Code Update : Added the toast message to show the timer up message to the user.

--- NewTimeFighter/app/src/main/java/com/example/newtimefighter/MainActivity.kt
+++ NewTimeFighter/app/src/main/java/com/example/newtimefighter/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,7 +18,7 @@ class MainActivity : AppCompatActivity() {
     internal var gameStarted = false
 
     internal lateinit var countDownTimer: CountDownTimer
-    internal val initialCountDown: Long = 60000
+    internal val initialCountDown: Long = 10000
     internal var countDownInterval: Long = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,14 +47,27 @@ class MainActivity : AppCompatActivity() {
                 timeLeftTextView.text = getString(R.string.timeLeft, timeLeft)
             }
             override fun onFinish() {
+                endGame()
             }
         }
         gameStarted = false
     }
 
     private fun incrementScore() {
+        if(!gameStarted) {
+            startGame()
+        }
         score += 1
         val newScore = getString(R.string.yourScore, score)
         gameScoreTextView.text = newScore
+    }
+
+    private fun startGame() {
+        countDownTimer.start()
+        gameStarted = true
+    }
+
+    private fun endGame() {
+        Toast.makeText(this, getString(R.string.gameOverMessage,score), Toast.LENGTH_LONG).show()
     }
 }
